aa8224e7d948fb19de30b2be6cdd7b3471969caf
zahraa77 <alzahraa553@gmail.com>
2018-08-14 10:58:24 +0200

    handling 404 error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="error">
+  <div class="row">
+      <div class="col-sm-14">
+
+
+          <img src="{{ asset('errors/404.png') }}" alt="not found" class="img-thumbnail">
+
+      </div>
+  </div>
+</div>
+@stop
